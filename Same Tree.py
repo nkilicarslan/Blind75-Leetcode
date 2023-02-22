a3d809750c10f9004d6fa1f268385606cb056edf
Necat Kılıçarslan <necatkilicarslann@gmail.com>
2023-02-22 14:04:29 +0300

    Add problem 100

--- Same Tree.py
+++ Same Tree.py
@@ -6,11 +6,17 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # check both root reached null or not
         if p == None and q == None:
+            # if both of them null or none return true
             return True
+        # check one of the root reached null or not
         elif p == None or q == None:
+            # if one of them null and other one is not null return false
             return False
+        # if both node value is equal, go on to recursion
         elif p.val == q.val:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         else:
+        # if the value of the nodes are not equal return false
             return False
